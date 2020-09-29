42cbfd77b0e49954c02de6c2649f251d54b4bcb3
stzn <sztk1209@gmail.com>
2020-09-30 08:29:44 +0900

    add XCTFail to remind us to call `assert` after recording a snapshot

--- PaginationNewsiOSTests/Helpers/XCTestCase+Snapshot.swift
+++ PaginationNewsiOSTests/Helpers/XCTestCase+Snapshot.swift
@@ -38,6 +38,8 @@ extension XCTestCase {
         do {
             try FileManager.default.createDirectory(at: snapshotURL.deletingLastPathComponent(), withIntermediateDirectories: true)
             try? snapshotData?.write(to: snapshotURL)
+
+            XCTFail("Record succeeded - use `assert` to compare the snapshot from now on.", file: file, line: line)
         } catch {
             XCTFail("Failed to record snapshot with error: \(error)",
                     file: file, line: line)
