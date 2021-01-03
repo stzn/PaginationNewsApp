c274188e13a0cf22d59f780193df984e15b31230
stzn <sztk1209@gmail.com>
2021-01-03 10:31:17 +0900

    add languageCode

--- PaginationNews/Articles API/SearchArticlesEndpoint.swift
+++ PaginationNews/Articles API/SearchArticlesEndpoint.swift
@@ -8,17 +8,20 @@
 import Foundation
 
 public enum SearchArticlesEndpoint {
-    case get(keyword: String, page: Int,
-             pageSize: Int = APIConstants.articlesPerPageCount)
+    case get(keyword: String,
+             page: Int,
+             pageSize: Int = APIConstants.articlesPerPageCount,
+             language: String = Locale.autoupdatingCurrent.languageCode ?? "en")
     public func url(baseURL: URL = URL(string: APIConstants.baseURL)!) -> URL {
         switch self {
-        case let .get(keyword, page, pageSize):
+        case let .get(keyword, page, pageSize, language):
             let remoteURL = baseURL.appendingPathComponent("everything")
             var compoents = URLComponents(url: remoteURL, resolvingAgainstBaseURL: false)!
             compoents.queryItems = [
                 URLQueryItem(name: "q", value: keyword),
                 URLQueryItem(name: "page", value: String(page)),
                 URLQueryItem(name: "pageSize", value: String(pageSize)),
+                URLQueryItem(name: "language", value: language),
                 URLQueryItem(name: "apiKey", value: APIConstants.apiKey),
             ]
             return compoents.url!
