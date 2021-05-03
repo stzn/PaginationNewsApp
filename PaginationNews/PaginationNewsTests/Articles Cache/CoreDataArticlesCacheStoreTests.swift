56ba98cc4bafd5fad06c41e8e2408043f4b40fae
stzn <sztk1209@gmail.com>
2021-05-03 13:55:17 +0900

    remove duplication

--- PaginationNews/PaginationNewsTests/Articles Cache/CoreDataArticlesCacheStoreTests.swift
+++ PaginationNews/PaginationNewsTests/Articles Cache/CoreDataArticlesCacheStoreTests.swift
@@ -12,43 +12,25 @@ class CoreDataArticlesCacheStoreTests: XCTestCase {
 	func test_retrieve_deliversEmptyOnEmptyCache() throws {
 		let sut = makeSUT()
 
-		let articles = try sut.retrieve()
-
-		XCTAssertNil(articles)
+		try assertEmptyCache(on: sut, retriveCount: .once)
 	}
 
 	func test_retrieve_hasNoSideEffectsOnEmptyCache() throws {
 		let sut = makeSUT()
 
-		_ = try sut.retrieve()
-		let articles = try sut.retrieve()
-
-		XCTAssertNil(articles)
+		try assertEmptyCache(on: sut, retriveCount: .twice)
 	}
 
 	func test_retrieve_deliversFoundValuesOnNonEmptyCache() throws {
-		let articles = [uniqueArticle]
-		let timestamp = Date()
 		let sut = makeSUT()
 
-		try sut.save(articles, timestamp)
-		let cached = try sut.retrieve()
-
-		XCTAssertEqual(cached?.articles, articles)
-		XCTAssertEqual(cached?.timestamp, timestamp)
+		try asserttNonEmptyCache(on: sut, retriveCount: .once)
 	}
 
 	func test_retrieve_hasNoSideEffectsOnNonEmptyCache() throws {
-		let articles = [uniqueArticle]
-		let timestamp = Date()
 		let sut = makeSUT()
 
-		try sut.save(articles, timestamp)
-		_ = try sut.retrieve()
-		let cached = try sut.retrieve()
-
-		XCTAssertEqual(cached?.articles, articles)
-		XCTAssertEqual(cached?.timestamp, timestamp)
+		try asserttNonEmptyCache(on: sut, retriveCount: .twice)
 	}
 
 	// MARK: - Helpers
@@ -58,5 +40,48 @@ class CoreDataArticlesCacheStoreTests: XCTestCase {
 		let sut = try! CoreDataArticlesCacheStore(storeURL: storeURL)
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return sut
+	}
+
+	private func assertEmptyCache(
+		on sut: CoreDataArticlesCacheStore,
+		retriveCount: RetrieveCount,
+		file: StaticString = #filePath, line: UInt = #line) throws {
+		let cached = try sut.retrieve(count: retriveCount)
+
+		XCTAssertNil(cached)
+	}
+
+	private func asserttNonEmptyCache(
+		on sut: CoreDataArticlesCacheStore,
+		retriveCount: RetrieveCount,
+		file: StaticString = #filePath, line: UInt = #line) throws {
+		let articles = [uniqueArticle]
+		let timestamp = Date()
+
+		try sut.save(articles, timestamp)
+		let cached = try sut.retrieve(count: retriveCount)
+
+		XCTAssertEqual(cached?.articles, articles)
+		XCTAssertEqual(cached?.timestamp, timestamp)
+	}
+}
+
+private enum RetrieveCount {
+	case once, twice
+}
+
+private extension CoreDataArticlesCacheStore {
+	func retrieve(count: RetrieveCount) throws -> CachedArticles? {
+		switch count {
+		case .once:
+			return try retrieve()
+		case .twice:
+			return try retrieveTwice()
+		}
+	}
+
+	func retrieveTwice() throws -> CachedArticles? {
+		_ = try retrieve()
+		return try retrieve()
 	}
 }
