868c60bb6209d311adc731033f6e2170bba37d22
stzn <sztk1209@gmail.com>
2021-05-05 13:46:09 +0900

    reorganize projects

---
+++ PaginationNews/PaginationNews/Articles API/TopHeadlineCategory.swift
@@ -0,0 +1,18 @@
+//
+//  TopHeadlineCategory.swift
+//  PaginationNews
+//
+//  Created by Shinzan Takata on 2021/01/03.
+//
+
+import Foundation
+
+public enum TopHeadlineCategory: String {
+	case business
+	case entertainment
+	case general
+	case health
+	case science
+	case sports
+	case technology
+}
